594c25e9af519478ded4d5df4736065fec6cd7c8
Ophelie Le Mentec <olementec@microsoft.com>
2022-04-13 16:30:52 +0200

    Added keyvault & storage account in Terraform. (#44)
    Added keyvault.
    
    Co-authored-by: Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
    Co-authored-by: Alexandre Gattiker <algattik@microsoft.com>
    Co-authored-by: Alexandre Gattiker <algattik@users.noreply.github.com>

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -11,6 +11,12 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_subscription" "current_subscription" {
+}
+
+data "azurerm_client_config" "current_client" {
+}
+
 resource "azurerm_resource_group" "participant" {
   name     = var.resource_group
   location = var.location
@@ -50,4 +56,49 @@ resource "azurerm_container_group" "edc" {
       protocol = "TCP"
     }
   }
+}
+
+resource "azurerm_key_vault" "participant" {
+  // added `kv` prefix because the keyvault name needs to begin with a letter
+  name                        = "kv${var.prefix}${var.participant_name}"
+  location                    = azurerm_resource_group.participant.location
+  resource_group_name         = azurerm_resource_group.participant.name
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current_client.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  enable_rbac_authorization   = true
+}
+
+# Role assignment so that the application may access the vault
+resource "azurerm_role_assignment" "edc_keyvault" {
+  scope                = azurerm_key_vault.participant.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = var.application_sp_object_id
+}
+
+# Role assignment so that the currently logged in user may add secrets to the vault
+resource "azurerm_role_assignment" "current-user-secretsofficer" {
+  scope                = azurerm_key_vault.participant.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current_client.object_id
+}
+
+resource "azurerm_storage_account" "assets" {
+  name                     = "${var.prefix}${var.participant_name}"
+  resource_group_name      = azurerm_resource_group.participant.name
+  location                 = azurerm_resource_group.participant.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+}
+
+resource "azurerm_key_vault_secret" "asset_storage_key" {
+  name         = "${azurerm_storage_account.assets.name}-key1"
+  value        = azurerm_storage_account.assets.primary_access_key
+  key_vault_id = azurerm_key_vault.participant.id
+  depends_on = [
+    azurerm_role_assignment.current-user-secretsofficer
+  ]
 }
