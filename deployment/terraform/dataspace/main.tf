19c927dbfdb0e05ed5024f46242d217bacc13632
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-06-10 13:58:25 +0200

    Registry Service DID (#211)
    * Add did to registry service
    
    * Update to add val
    
    * Update url
    
    * Update
    
    * udpate
    
    * tf fmt
    
    * update env
    
    * remove identity hub from authority did - pr feedback
    
    * use step output - pr feedback
    
    * Update deployment/terraform/dataspace/main.tf
    
    Co-authored-by: Alexandre Gattiker <algattik@users.noreply.github.com>
    
    * terraform reformat
    
    Co-authored-by: Alexandre Gattiker <algattik@users.noreply.github.com>

--- deployment/terraform/dataspace/main.tf
+++ deployment/terraform/dataspace/main.tf
@@ -47,6 +47,8 @@ locals {
 
   connector_name = "connector-registry"
 
+  did_url = "did:web:${azurerm_storage_account.did.primary_web_host}"
+
   registry_service_dns_label = "${var.prefix}-registry-mvd"
   edc_default_port           = 8181
 }
@@ -110,5 +112,82 @@ resource "azurerm_container_group" "registry-service" {
       }
     }
   }
+}
+
+resource "azurerm_key_vault" "registry" {
+  // added `kv` prefix because the keyvault name needs to begin with a letter
+  name                        = "kv${var.prefix}-registry"
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.dataspace.name
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current_client.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  enable_rbac_authorization   = true
+}
+
+# Role assignment so that the application may access the vault
+resource "azurerm_role_assignment" "registry_keyvault" {
+  scope                = azurerm_key_vault.registry.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = var.application_sp_object_id
+}
+
+# Role assignment so that the currently logged in user may add secrets to the vault
+resource "azurerm_role_assignment" "current-user-secretsofficer" {
+  scope                = azurerm_key_vault.registry.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current_client.object_id
+}
+
+resource "azurerm_storage_account" "did" {
+  name                     = "${var.prefix}did"
+  resource_group_name      = azurerm_resource_group.dataspace.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  static_website {}
+}
+
+resource "azurerm_key_vault_secret" "did_key" {
+  name = local.connector_name
+  # Create did_key secret only if key_file value is provided. Default key_file value is null.
+  count        = var.key_file == null ? 0 : 1
+  value        = file(var.key_file)
+  key_vault_id = azurerm_key_vault.registry.id
+  depends_on = [
+    azurerm_role_assignment.current-user-secretsofficer
+  ]
+}
+
+resource "azurerm_storage_blob" "did" {
+  name                 = ".well-known/did.json" # `.well-known` path is defined by did:web specification
+  storage_account_name = azurerm_storage_account.did.name
+  # Create did blob only if public_key_jwk_file is provided. Default public_key_jwk_file value is null.
+  count                  = var.public_key_jwk_file == null ? 0 : 1
+  storage_container_name = "$web" # container used to serve static files (see static_website property on storage account)
+  type                   = "Block"
+  source_content = jsonencode({
+    id = local.did_url
+    "@context" = [
+      "https://www.w3.org/ns/did/v1",
+      {
+        "@base" = local.did_url
+      }
+    ],
+    "verificationMethod" = [
+      {
+        "id"           = "#identity-key-1"
+        "controller"   = ""
+        "type"         = "JsonWebKey2020"
+        "publicKeyJwk" = jsondecode(file(var.public_key_jwk_file))
+      }
+    ],
+    "authentication" : [
+      "#identity-key-1"
+  ] })
+  content_type = "application/json"
 }
 
