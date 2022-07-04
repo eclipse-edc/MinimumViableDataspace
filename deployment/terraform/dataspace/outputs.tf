2827a86559fc66608be2bdd96c00b5bb293e97e0
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-07-04 20:45:45 +0200

    Deploy Gaia-X Authority did document (#175)

--- deployment/terraform/dataspace/outputs.tf
+++ deployment/terraform/dataspace/outputs.tf
@@ -7,6 +7,10 @@ output "registration_service_url" {
   value = "http://${azurerm_container_group.registration-service.fqdn}:${local.edc_default_port}"
 }
 
-output "did_host" {
-  value = length(azurerm_storage_blob.did) > 0 ? azurerm_storage_account.did.primary_web_host : null
+output "authority_did_host" {
+  value = length(azurerm_storage_blob.authority_did) > 0 ? azurerm_storage_account.authority_did.primary_web_host : null
+}
+
+output "gaiax_did_host" {
+  value = length(azurerm_storage_blob.gaiax_did) > 0 ? azurerm_storage_account.gaiax_did.primary_web_host : null
 }
