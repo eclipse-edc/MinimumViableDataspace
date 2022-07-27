9362f36f09123d7ceca67b8239cae2b82bacca3a
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-07-27 11:17:24 +0200

    Cloud Deployment: Fix MVD cloud deployment health check failure (#30)
    * Cloud Deployment: Fix MVD cloud deployment health check failure (#217)
    
    * Update liveness check
    
    * Update liveness check
    
    * Upgrade curl
    
    * Update
    
    * upgrade curl
    
    * Update
    
    * update liveness probe
    
    * Update deployment/curl-upgrade.sh
    
    Co-authored-by: Izabela Kulakowska <ikulakowska@microsoft.com>
    
    * Fix typo
    
    Co-authored-by: Izabela Kulakowska <ikulakowska@microsoft.com>
    Co-authored-by: Max Zeier <33636338+zeier@users.noreply.github.com>
    
    * terraform fmt
    
    Co-authored-by: Izabela Kulakowska <ikulakowska@microsoft.com>
    Co-authored-by: Max Zeier <33636338+zeier@users.noreply.github.com>

--- deployment/terraform/participant/main.tf
+++ deployment/terraform/participant/main.tf
@@ -128,7 +128,9 @@ resource "azurerm_container_group" "edc" {
         port = 8181
         path = "/api/check/health"
       }
-      failure_threshold = 6
+      initial_delay_seconds = 10
+      failure_threshold     = 6
+      timeout_seconds       = 3
     }
   }
 }
