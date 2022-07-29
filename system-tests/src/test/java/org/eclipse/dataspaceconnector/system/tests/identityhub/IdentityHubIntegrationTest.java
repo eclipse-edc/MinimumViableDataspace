e70bb01fc6f63c2bdce8389fe9729b1303ab4227
Marc Gomez <marcgomez@microsoft.com>
2022-07-29 11:50:31 +0200

    Identity Hub: push region verifiable credential on deploy (#223)
    * Push region verifiable credential on deploy

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/identityhub/IdentityHubIntegrationTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/identityhub/IdentityHubIntegrationTest.java
@@ -15,18 +15,23 @@
 package org.eclipse.dataspaceconnector.system.tests.identityhub;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import com.nimbusds.jwt.SignedJWT;
 import okhttp3.OkHttpClient;
+import org.assertj.core.api.ObjectAssert;
 import org.eclipse.dataspaceconnector.identityhub.client.IdentityHubClientImpl;
+import org.eclipse.dataspaceconnector.spi.monitor.ConsoleMonitor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.eclipse.dataspaceconnector.system.tests.utils.TestUtils.requiredPropOrEnv;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -42,28 +47,57 @@ public class IdentityHubIntegrationTest {
             .readTimeout(1, TimeUnit.MINUTES)
             .build();
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final ConsoleMonitor CONSOLE_MONITOR = new ConsoleMonitor();
 
     private IdentityHubClientImpl client;
 
     @BeforeEach
     void setUp() {
-        client = new IdentityHubClientImpl(OK_HTTP_CLIENT, OBJECT_MAPPER);
+        client = new IdentityHubClientImpl(OK_HTTP_CLIENT, OBJECT_MAPPER, CONSOLE_MONITOR);
     }
 
     @ParameterizedTest
     @MethodSource("provideHubUrls")
-    void retrieveVerifiableCredentials_empty(String hubUrl) throws IOException {
+    void retrieveVerifiableCredentials(String hubUrl, String region) {
+        await().atMost(20, SECONDS).untilAsserted(() -> singleVcInIdentityHub(hubUrl));
+
+        singleVcInIdentityHub(hubUrl)
+                .satisfies(jwt -> {
+                    var claims = jwt.getJWTClaimsSet();
+                    assertThat(claims.getIssuer()).as("Issuer is a Web DID").startsWith("did:web:");
+                    assertThat(claims.getSubject()).as("Subject is a Web DID").startsWith("did:web:");
+                    assertThat(claims.getClaim("vc")).as("VC")
+                            .isInstanceOfSatisfying(JSONObject.class, t -> {
+
+                                assertThat(t.get("id"))
+                                        .as("VC ID")
+                                        .isInstanceOfSatisfying(String.class, s -> assertThat(s).isNotBlank());
+
+                                assertThat(t.get("credentialSubject"))
+                                        .as("VC credentialSubject")
+                                        .isInstanceOfSatisfying(JSONObject.class,
+                                                s -> assertThat(s.get("region"))
+                                                        .as("region")
+                                                        .isInstanceOfSatisfying(String.class,
+                                                                r -> assertThat(r).isEqualTo(region)));
+
+                            })
+                    ;
+                });
+    }
+
+    private ObjectAssert<SignedJWT> singleVcInIdentityHub(String hubUrl) {
         var vcs = client.getVerifiableCredentials(hubUrl);
 
         assertThat(vcs.succeeded()).isTrue();
-        assertThat(vcs.getContent()).isEmpty();
+        return assertThat(vcs.getContent()).singleElement();
     }
 
     private static Stream<Arguments> provideHubUrls() {
         return Stream.of(
-                arguments(PROVIDER_IDENTITY_HUB_URL),
-                arguments(CONSUMER_EU_IDENTITY_HUB_URL),
-                arguments(CONSUMER_US_IDENTITY_HUB_URL)
+                arguments(PROVIDER_IDENTITY_HUB_URL, "eu"),
+                arguments(CONSUMER_EU_IDENTITY_HUB_URL, "eu"),
+                arguments(CONSUMER_US_IDENTITY_HUB_URL, "us")
         );
     }
 }
