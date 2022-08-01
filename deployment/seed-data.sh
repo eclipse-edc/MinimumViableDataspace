78a4882f13ecc4ce6b246c561874b042934d7525
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-08-01 10:43:07 +0200

    added participant_id suffix to assets

--- deployment/seed-data.sh
+++ deployment/seed-data.sh
@@ -6,5 +6,6 @@ newman run \
   --folder "Publish Master Data" \
   --env-var data_management_url="http://$EDC_HOST:9191/api/v1/data" \
   --env-var storage_account="$ASSETS_STORAGE_ACCOUNT" \
+  --env-var participant_id="$PARTICIPANT_ID"
   --env-var api_key="$API_KEY" \
   deployment/data/MVD.postman_collection.json
