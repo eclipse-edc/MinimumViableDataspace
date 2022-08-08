b6c76d5e29906c743c8c224cb953e081894a3d2c
Marc Gomez <marcgomez@microsoft.com>
2022-08-08 18:08:57 +0200

    IdentityHub: Use IdentityHubCredentialsVerifier (#239) (#43)
    * IdentityHub: Use IdentityHubCredentialsVerifier (#239)
    
    * Use IdentityHubCredentialsVerifier
    
    * Changelog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,4 @@ rootProject.name = "mvd"
 include(":launcher")
 include(":system-tests")
 include(":extensions:refresh-catalog")
-include(":extensions:mock-credentials-verifier")
 include(":extensions:policies")
