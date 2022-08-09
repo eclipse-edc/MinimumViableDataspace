8b74ff94b79c4f4bc97f354bde59930c082bb2b5
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-08-09 14:12:11 +0200

    Update Local Setup to make it more consistent to cloud setup. (#242)
    * update local setup to keep it consistent with cloud setup
    
    Update names
    
    Update
    
    Update kv
    
    working
    
    Add seed data to all connectors
    
    Update README
    
    * Update system-tests/README.md
    
    Co-authored-by: Alexandre Gattiker <algattik@users.noreply.github.com>
    
    * Update system-tests/README.md
    
    Co-authored-by: Alexandre Gattiker <algattik@users.noreply.github.com>
    
    * Update to use same delimiter in shell script as used in other shell script  - prf feedback
    
    * Update README.md
    
    Co-authored-by: Alexandre Gattiker <algattik@users.noreply.github.com>
    Co-authored-by: Alexandre Gattiker <algattik@microsoft.com>

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/local/BlobTransferIntegrationTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/local/BlobTransferIntegrationTest.java
@@ -38,9 +38,9 @@ public class BlobTransferIntegrationTest extends AbstractBlobTransferTest {
     private List<Runnable> containerCleanup = new ArrayList<>();
     private static final String PROVIDER_CONTAINER_NAME = "src-container";
     public static final String LOCAL_BLOB_STORE_ENDPOINT_TEMPLATE = "http://127.0.0.1:10000/%s";
-    public static final String LOCAL_SOURCE_BLOB_STORE_ACCOUNT = "providerassets";
+    public static final String LOCAL_SOURCE_BLOB_STORE_ACCOUNT = "company1assets";
     public static final String LOCAL_SOURCE_BLOB_STORE_ACCOUNT_KEY = "key1";
-    public static final String LOCAL_DESTINATION_BLOB_STORE_ACCOUNT = "consumereuassets";
+    public static final String LOCAL_DESTINATION_BLOB_STORE_ACCOUNT = "company2assets";
     public static final String LOCAL_DESTINATION_BLOB_STORE_ACCOUNT_KEY = "key2";
 
 
