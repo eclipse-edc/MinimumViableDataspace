badba1481dfecf000608a787c4b1fde91ee2ef8c
Alexandre Gattiker <algattik@users.noreply.github.com>
2022-08-09 09:44:47 +0200

    Push GAIA-X membership Verifiable Credential (with ADR)

--- system-tests/resources/cli-tools/entrypoint.sh
+++ system-tests/resources/cli-tools/entrypoint.sh
@@ -16,11 +16,16 @@ for participant in "${PARTICIPANTS[@]}"; do
     echo "Registering $participantName"
     java -jar registration-service-cli.jar -d="$did" -k=/resources/vault/$participantName/private-key.pem -s='http://registration-service:8184/authority' participants add --ids-url "http://$participantName:8282"
 
-    echo "Seeding VC for $participantName"
-    vcId=$(uuidgen)
-    java -jar identity-hub-cli.jar -s="http://$participantName:8181/api/identity-hub" vc add \
-                -c='{"id":"'$vcId'","credentialSubject":{"region":"'$region'"}}' \
-                -b="$did" \
-                -i="did:web:did-server:gaia-x" \
-                -k="/resources/vault/gaia-x/private-key.pem"
+   for subject in '"region": "'$region'"' '"gaiaXMember": "true"'
+   do
+     echo "Seeding VC for $participantName: $subject"
+     vcId=$(uuidgen)
+     java -jar identity-hub-cli.jar \
+                 -s="http://$participantName:8181/api/identity-hub" \
+                 vc add \
+                 -c='{"id":"'$vcId'","credentialSubject":{'"$subject"}'}' \
+                 -b="$did" \
+                 -i="did:web:did-server:gaia-x" \
+                 -k="/resources/vault/gaia-x/private-key.pem"
+   done
 done
