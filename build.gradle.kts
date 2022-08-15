28804a72895f65a1f23d929631301d974f8da562
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-08-15 13:43:34 +0200

    Add codeql and checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,21 @@ plugins {
     java
     `java-library`
     jacoco
+    checkstyle
 }
 
 allprojects {
     apply(plugin = "java")
+    apply(plugin = "checkstyle")
     if (System.getenv("JACOCO") == "true") {
         apply(plugin = "jacoco")
+    }
+
+    checkstyle {
+        toolVersion = "9.0"
+        configFile = rootProject.file("resources/checkstyle-config.xml")
+        configDirectory.set(rootProject.file("resources"))
+        maxErrors = 0 // does not tolerate errors
     }
 
     repositories {
