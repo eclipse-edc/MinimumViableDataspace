e1d3ff57db1764504e38bf072499e93bb032e565
Peter Kirchner <pkirch@users.noreply.github.com>
2022-08-25 09:28:02 +0200

    Verify participant onboarding status (#71)
    Verify participant onboarding status (#263)
    
    - As the registration service now has an endpoint to fetch the record of a participant, this can be utilized during participant onboarding to verify that onboarding completed successfully or not.
    - Update to system-tests/README.md necessary for a successful build of Registration service
    - Add health check to system-tests/resources/cli-tools/Dockerfile to have an indicator when container cli-tools is finished.
    - Updated to use latest version of registration service.
    
    Co-authored-by: Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>

--- system-tests/resources/cli-tools/entrypoint.sh
+++ system-tests/resources/cli-tools/entrypoint.sh
@@ -3,34 +3,76 @@
 # stop on error
 set -euo pipefail
 
+function registerParticipant() {
+  local participantName="$1"
+  local participantDid="$2"
+
+  echo "Registering $participantName"
+  java -jar registration-service-cli.jar \
+              -d="did:web:did-server:registration-service" \
+              --http-scheme \
+              -k=/resources/vault/$participantName/private-key.pem \
+              -c="$participantDid" \
+               participants add
+}
+
+function seedVerifiedCredentials() {
+  local participantName="$1"
+  local participantDid="$2"
+  local region="$3"
+
+   for subject in '"region": "'$region'"' '"gaiaXMember": "true"'
+   do
+     echo "Seeding VC for $participantName: $subject"
+     vcId=$(uuidgen)
+     java -jar identity-hub-cli.jar \
+                 -s="http://$participantName:8181/api/identity-hub" \
+                 vc add \
+                 -c='{"id":"'$vcId'","credentialSubject":{'"$subject"'}}' \
+                -b="$participantDid" \
+                 -i="did:web:did-server:gaia-x" \
+                 -k="/resources/vault/gaia-x/private-key.pem"
+   done
+}
+
 PARTICIPANTS=(company1:eu company2:eu company3:us)
 
-# Register dataspace participants
+# Seed VCs and register participants.
 for participant in "${PARTICIPANTS[@]}"; do
-    participantArray=(${participant//:/ })
+  participantArray=(${participant//:/ })
 
-    participantName=${participantArray[0]}
-    region=${participantArray[1]}
-    participantDid="did:web:did-server:$participantName"
+  participantName=${participantArray[0]}
+  region=${participantArray[1]}
+  participantDid="did:web:did-server:$participantName"
 
-    for subject in '"region": "'$region'"' '"gaiaXMember": "true"'
-    do
-         echo "Seeding VC for $participantName: $subject"
-         vcId=$(uuidgen)
-         java -jar identity-hub-cli.jar \
-                -s="http://$participantName:8181/api/identity-hub" \
-                 vc add \
-                -c='{"id":"'$vcId'","credentialSubject":{'"$subject"}'}' \
-                -b="$participantDid" \
-                -i="did:web:did-server:gaia-x" \
-                -k="/resources/vault/gaia-x/private-key.pem"
-    done
+  # seed vc for participant
+  seedVerifiedCredentials "$participantName" "$participantDid" "$region"
 
-    echo "Registering $participantName"
-    java -jar registration-service-cli.jar \
-                -d="did:web:did-server:registration-service" \
-                --http-scheme \
-                -k=/resources/vault/$participantName/private-key.pem \
-                -c="$participantDid" \
-                 participants add
+  # Register dataspace participants
+  registerParticipant "$participantName" "$participantDid"
 done
+
+# Await registrations of participants.
+for participant in "${PARTICIPANTS[@]}"; do
+  participantArray=(${participant//:/ })
+
+  participantName=${participantArray[0]}
+  participantDid="did:web:did-server:$participantName"
+
+  cmd="java -jar registration-service-cli.jar \
+                  -d=did:web:did-server:registration-service \
+                  --http-scheme \
+                  -k=/resources/vault/$participantName/private-key.pem \
+                  -c=$participantDid \
+                  participants get"
+
+  # Wait for participant registration.
+  ./validate_onboarding.sh "$participantDid" "$cmd"
+done
+
+# flag for healthcheck by Docker
+echo "finished" > finished.flag
+echo "Finished successfully! Keep the container running."
+
+# keep the container running
+sleep infinity
