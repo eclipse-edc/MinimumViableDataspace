c020a789109c26bd6fc2998a37e74917d054fb84
Benjamin Scholtes <88310985+bscholtes1A@users.noreply.github.com>
2022-08-30 11:59:16 +0200

    feat: Add link to self-description in DID document (#76)
    Add link to self-description in DID document (#278)

--- deployment/terraform/dataspace/main.tf
+++ deployment/terraform/dataspace/main.tf
@@ -164,64 +164,6 @@ resource "azurerm_role_assignment" "current-user-secretsofficer" {
   principal_id         = data.azurerm_client_config.current_client.object_id
 }
 
-# Internal Dataspace Authority resources (Dataspace DID)
-resource "azurerm_storage_account" "dataspace_did" {
-  name                     = "${var.prefix}dataspacedid"
-  resource_group_name      = azurerm_resource_group.dataspace.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  static_website {}
-}
-
-resource "azurerm_storage_blob" "dataspace_did" {
-  name                 = ".well-known/did.json" # `.well-known` path is defined by did:web specification
-  storage_account_name = azurerm_storage_account.dataspace_did.name
-  # Create did blob only if public_key_jwk_file is provided. Default public_key_jwk_file value is null.
-  count                  = var.public_key_jwk_file_authority == null ? 0 : 1
-  storage_container_name = "$web" # container used to serve static files (see static_website property on storage account)
-  type                   = "Block"
-  source_content = jsonencode({
-    id = local.dataspace_did_uri
-    "@context" = [
-      "https://www.w3.org/ns/did/v1",
-      {
-        "@base" = local.dataspace_did_uri
-      }
-    ],
-    "service" : [
-      {
-        "id" : "#registration-url",
-        "type" : "RegistrationUrl",
-        "serviceEndpoint" : local.registration_service_url
-      }
-    ],
-    "verificationMethod" = [
-      {
-        "id"           = "#identity-key-authority"
-        "controller"   = ""
-        "type"         = "JsonWebKey2020"
-        "publicKeyJwk" = jsondecode(file(var.public_key_jwk_file_authority))
-      }
-    ],
-    "authentication" : [
-      "#identity-key-authority"
-  ] })
-  content_type = "application/json"
-}
-
-# GAIA-X Authority resources
-resource "azurerm_storage_account" "gaiax_did" {
-  name                     = "${var.prefix}gaiaxdid"
-  resource_group_name      = azurerm_resource_group.dataspace.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  static_website {}
-}
-
 resource "azurerm_storage_account" "shared" {
   name                     = "${var.prefix}${local.dataspace_authority_name}shared"
   resource_group_name      = azurerm_resource_group.dataspace.name
@@ -236,34 +178,3 @@ resource "azurerm_storage_share" "share" {
   storage_account_name = azurerm_storage_account.shared.name
   quota                = 1
 }
-
-resource "azurerm_storage_blob" "gaiax_did" {
-  name                 = ".well-known/did.json" # `.well-known` path is defined by did:web specification
-  storage_account_name = azurerm_storage_account.gaiax_did.name
-  # Create did blob only if public_key_jwk_file is provided. Default public_key_jwk_file value is null.
-  count                  = var.public_key_jwk_file_gaiax == null ? 0 : 1
-  storage_container_name = "$web" # container used to serve static files (see static_website property on storage account)
-  type                   = "Block"
-  source_content = jsonencode({
-    id = local.gaiax_did_uri
-    "@context" = [
-      "https://www.w3.org/ns/did/v1",
-      {
-        "@base" = local.gaiax_did_uri
-      }
-    ],
-    "service" : [],
-    "verificationMethod" = [
-      {
-        "id"           = "#identity-key-gaiax"
-        "controller"   = ""
-        "type"         = "JsonWebKey2020"
-        "publicKeyJwk" = jsondecode(file(var.public_key_jwk_file_gaiax))
-      }
-    ],
-    "authentication" : [
-      "#identity-key-gaiax"
-  ] })
-  content_type = "application/json"
-}
-
