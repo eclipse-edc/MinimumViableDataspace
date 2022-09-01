5deccb26010f51ea38e77860a64720465fba3d2e
Alexandre Gattiker <algattik@microsoft.com>
2022-09-01 13:50:43 +0200

    .

--- resources/setup_azure_ad/set-gh-secrets.sh
+++ resources/setup_azure_ad/set-gh-secrets.sh
@@ -3,7 +3,6 @@ gh="gh --repo $REPO"
 
 $gh secret set ACR_NAME --body "$(terraform output -raw ACR_NAME)"
 $gh secret set ARM_CLIENT_ID --body "$(terraform output -raw ARM_CLIENT_ID)"
-$gh secret set ARM_CLIENT_SECRET --body "$(terraform output -raw ARM_CLIENT_SECRET)"
 $gh secret set ARM_SUBSCRIPTION_ID --body "$(terraform output -raw ARM_SUBSCRIPTION_ID)"
 $gh secret set ARM_TENANT_ID --body "$(terraform output -raw ARM_TENANT_ID)"
 $gh secret set APP_CLIENT_ID --body "$(terraform output -raw APP_CLIENT_ID)"
