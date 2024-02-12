0a731a54eaeed751f33f012734f2d0479a2176a2
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-02-12 19:20:27 +0100

    wip

--- deployment/modules/connector/identityhub.tf
+++ deployment/modules/connector/identityhub.tf
@@ -65,19 +65,22 @@ resource "kubernetes_config_map" "identityhub-config" {
 
   data = {
     # IdentityHub variables
-    EDC_API_AUTH_KEY          = "password"
-    EDC_IH_IAM_ID             = var.participantId
-    EDC_IAM_DID_WEB_USE_HTTPS = false
-    EDC_IH_IAM_PUBLICKEY_PEM  = var.publickey-pem
-    EDC_IH_API_SUPERUSER_KEY  = var.ih_superuser_apikey
-    WEB_HTTP_PORT             = var.ports.ih-default
-    WEB_HTTP_PATH             = "/api"
-    WEB_HTTP_MANAGEMENT_PORT  = var.ports.ih-management
-    WEB_HTTP_MANAGEMENT_PATH  = "/api/management"
-    WEB_HTTP_RESOLUTION_PORT  = var.ports.resolution-api
-    WEB_HTTP_RESOLUTION_PATH  = "/api/resolution"
-    WEB_HTTP_DID_PORT         = var.ports.ih-did
-    WEB_HTTP_DID_PATH         = "/"
-    JAVA_TOOL_OPTIONS         = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${var.ports.ih-debug}"
+    EDC_API_AUTH_KEY             = "password"
+    EDC_IH_IAM_ID                = var.participantId
+    EDC_IAM_DID_WEB_USE_HTTPS    = false
+    EDC_IH_IAM_PUBLICKEY_PEM     = var.publickey-pem
+    EDC_IH_API_SUPERUSER_KEY     = var.ih_superuser_apikey
+    WEB_HTTP_PORT                = var.ports.ih-default
+    WEB_HTTP_PATH                = "/api"
+    WEB_HTTP_MANAGEMENT_PORT     = var.ports.ih-management
+    WEB_HTTP_MANAGEMENT_PATH     = "/api/management"
+    WEB_HTTP_RESOLUTION_PORT     = var.ports.resolution-api
+    WEB_HTTP_RESOLUTION_PATH     = "/api/resolution"
+    WEB_HTTP_DID_PORT            = var.ports.ih-did
+    WEB_HTTP_DID_PATH            = "/"
+    JAVA_TOOL_OPTIONS            = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${var.ports.ih-debug}"
+    EDC_IAM_STS_PRIVATEKEY_ALIAS = "key-1"
+    EDC_IAM_STS_PUBLICKEY_ALIAS  = "${var.participant-did}#key-1"
+    EDC_MVD_CREDENTIALS_PATH     = "credentials/k8s/${var.humanReadableName}"
   }
 }
