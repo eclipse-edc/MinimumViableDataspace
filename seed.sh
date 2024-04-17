a6eee5547a3efe4f52e9c1f4e6dbe47db1c79f20
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-04-17 11:44:19 +0200

    feat: add script to sign existing VCs

--- seed.sh
+++ seed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Seed application data to both connectors
+## Seed application DATA to both connectors
 for url in 'http://127.0.0.1:8081' 'http://127.0.0.1:8091'
 do
   newman run --folder "Seed" \
@@ -8,51 +8,57 @@ do
     ./deployment/postman/MVD.postman_collection.json > /dev/null
 done
 
-## Seed management data to identityhubs
+## Seed management DATA to identityhubs
 API_KEY="c3VwZXItdXNlcg==.c3VwZXItc2VjcmV0LWtleQo="
 
 # add participant alice
-curl --location 'http://localhost:7082/api/management/v1/participants/' \
+PEM_ALICE=$(sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' deployment/assets/alice_public.pem)
+DATA_ALICE=$(jq -n --arg pem "$PEM_ALICE" '{
+           "roles":[],
+           "serviceEndpoints":[
+             {
+                "type": "CredentialService",
+                "serviceEndpoint": "http://localhost:7081/api/resolution/v1/participants/ZGlkOndlYjpsb2NhbGhvc3QlM0E3MDgz",
+                "id": "alice-credentialservice-1"
+             }
+           ],
+           "active": true,
+           "participantId": "did:web:localhost%3A7083",
+           "did": "did:web:localhost%3A7083",
+           "key":{
+               "keyId": "key-1",
+               "privateKeyAlias": "key-1",
+               "publicKeyPem":"\($pem)"
+           }
+       }')
+
+curl -s --location 'http://localhost:7082/api/management/v1/participants/' \
 --header 'Content-Type: application/json' \
 --header "x-api-key: $API_KEY" \
---data '{
-    "roles":[],
-    "serviceEndpoints":[
-      {
-         "type": "CredentialService",
-         "serviceEndpoint": "http://localhost:7081/api/resolution/v1/participants/ZGlkOndlYjpsb2NhbGhvc3QlM0E3MDgz",
-         "id": "credentialservice-1"
-      }
-    ],
-    "active": true,
-    "participantId": "did:web:localhost%3A7083",
-    "did": "did:web:localhost%3A7083",
-    "key":{
-        "keyId": "key-1",
-        "privateKeyAlias": "key-1",
-        "publicKeyPem":"-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1l0Lof0a1yBc8KXhesAnoBvxZw5r\noYnkAXuqCYfNK3ex+hMWFuiXGUxHlzShAehR6wvwzV23bbC0tcFcVgW//A==\n-----END PUBLIC KEY-----"
-    }
-}'
+--data "$DATA_ALICE"
 
 # add participant bob
-curl --location 'http://localhost:7092/api/management/v1/participants/' \
+PEM_BOB=$(sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' deployment/assets/bob_public.pem)
+DATA_BOB=$(jq -n --arg pem "$PEM_BOB" '{
+            "roles":[],
+            "serviceEndpoints":[
+              {
+                 "type": "CredentialService",
+                 "serviceEndpoint": "http://localhost:7091/api/resolution/v1/participants/ZGlkOndlYjpsb2NhbGhvc3QlM0E3MDkz",
+                 "id": "bob-credentialservice-1"
+              }
+            ],
+            "active": true,
+            "participantId": "did:web:localhost%3A7093",
+            "did": "did:web:localhost%3A7093",
+            "key":{
+                "keyId": "key-1",
+                "privateKeyAlias": "key-1",
+                "publicKeyPem":"\($pem)"
+            }
+      }')
+
+curl -s --location 'http://localhost:7092/api/management/v1/participants/' \
 --header 'Content-Type: application/json' \
 --header "x-api-key: $API_KEY" \
---data '{
-    "roles":[],
-    "serviceEndpoints":[
-      {
-         "type": "CredentialService",
-         "serviceEndpoint": "http://localhost:7091/api/resolution/v1/participants/ZGlkOndlYjpsb2NhbGhvc3QlM0E3MDkz",
-         "id": "credentialservice-1"
-      }
-    ],
-    "active": true,
-    "participantId": "did:web:localhost%3A7093",
-    "did": "did:web:localhost%3A7093",
-    "key":{
-        "keyId": "key-1",
-        "privateKeyAlias": "key-1",
-        "publicKeyPem":"-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1l0Lof0a1yBc8KXhesAnoBvxZw5r\noYnkAXuqCYfNK3ex+hMWFuiXGUxHlzShAehR6wvwzV23bbC0tcFcVgW//A==\n-----END PUBLIC KEY-----"
-    }
-}'
+--data "$DATA_BOB"
