140f18c8f79d035bd42fb38e621f7a345f177875
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2025-04-07 07:23:31 +0200

    fix compile errors - again

--- extensions/catalog-node-resolver/src/main/java/org/eclipse/edc/demo/participants/resolver/LazyLoadNodeDirectory.java
+++ extensions/catalog-node-resolver/src/main/java/org/eclipse/edc/demo/participants/resolver/LazyLoadNodeDirectory.java
@@ -72,10 +72,6 @@ public class LazyLoadNodeDirectory implements TargetNodeDirectory {
         //noop
     }
 
-    @Override
-    public TargetNode remove(String s) {
-        return null;
-    }
 
     private TargetNode createNode(String name, String did) {
         var didResult = didResolverRegistry.resolve(did);
