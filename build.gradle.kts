21d757cb92a7658feda2c759932020e02e622186
ndr_brt <andrea.bertagnolli@gmail.com>
2025-10-29 10:02:22 +0100

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,52 +13,30 @@
  */
 
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin
 
 plugins {
     `java-library`
     id("com.bmuschko.docker-remote-api") version "9.4.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     alias(libs.plugins.edc.build)
-}
-
-buildscript {
-    dependencies {
-        val version: String by project
-        classpath("org.eclipse.edc.autodoc:org.eclipse.edc.autodoc.gradle.plugin:$version")
-    }
 }
 
 val edcBuildId = libs.plugins.edc.build.get().pluginId
 
 allprojects {
     apply(plugin = edcBuildId)
-    apply(plugin = "org.eclipse.edc.autodoc")
-
-    // configure which version of the annotation processor to use. defaults to the same version as the plugin
-    configure<org.eclipse.edc.plugins.autodoc.AutodocExtension> {
-        outputDirectory.set(project.layout.buildDirectory.asFile)
-    }
-
-    configure<org.eclipse.edc.plugins.edcbuild.extensions.BuildExtension> {
-        swagger {
-            title.set("Identity HUB REST API")
-            description = "Identity HUB REST APIs - merged by OpenApiMerger"
-            outputFilename.set(project.name)
-            outputDirectory.set(file("${rootProject.projectDir.path}/resources/openapi/yaml"))
-        }
-    }
 }
+
+
+val shadowPluginId = libs.plugins.shadow.get().pluginId
 subprojects {
     afterEvaluate {
-        if (project.plugins.hasPlugin("com.github.johnrengelman.shadow") &&
+        if (project.plugins.hasPlugin(shadowPluginId) &&
             file("${project.projectDir}/src/main/docker/Dockerfile").exists()
         ) {
-
             //actually apply the plugin to the (sub-)project
             apply(plugin = "com.bmuschko.docker-remote-api")
-            // configure the "dockerize" task
-            val dockerTask: DockerBuildImage = tasks.create("dockerize", DockerBuildImage::class) {
+
+            tasks.register("dockerize", DockerBuildImage::class) {
                 val dockerContextDir = project.projectDir
                 dockerFile.set(file("$dockerContextDir/src/main/docker/Dockerfile"))
                 images.add("${project.name}:${project.version}")
@@ -68,9 +46,8 @@ subprojects {
                     platform.set(System.getProperty("platform"))
                 buildArgs.put("JAR", "build/libs/${project.name}.jar")
                 inputDir.set(file(dockerContextDir))
+                dependsOn("shadowJar")
             }
-            // make sure  always runs after "dockerize" and after "copyOtel"
-            dockerTask.dependsOn(tasks.named(ShadowJavaPlugin.SHADOW_JAR_TASK_NAME))
         }
     }
 }
