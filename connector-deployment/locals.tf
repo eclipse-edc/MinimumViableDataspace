168c36064a4206fb279f247d0f40761aab0407a5
js <js@jshost.com>
2026-01-29 10:40:06 +0100

    fix identityhub did

--- connector-deployment/locals.tf
+++ connector-deployment/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  participant-did = "did:web:${var.participant}-identityhub%3A7083:${var.participant}"
+  participant-did = "did:web:${var.participant}-identityhub.${var.participant}%3A7083:${var.participant}"
   database_url    = "jdbc:postgresql://${var.postgres_endpoint}:${var.postgres_port}/${var.participant}"
   vault_url       = "http://${var.participant}-vault:8200"
 
