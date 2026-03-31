41db820c93bb2e8c9f29d2fa090e6ef5afda8e48
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2026-03-31 16:03:11 +0200

    build: add Docker build  (#592)
    * build: add docker image build and publish
    
    * add documentation
    
    * license headers
    
    * update image pull policy in e2e test
    
    * fix
    
    * increase timeout
    
    * add missing extension
    
    * fix tests, update gradle
    
    * fix grep

--- tests/end2end/src/test/java/org/eclipse/edc/demo/tests/issuance/CredentialIssuanceEndToEndTest.java
+++ tests/end2end/src/test/java/org/eclipse/edc/demo/tests/issuance/CredentialIssuanceEndToEndTest.java
@@ -37,7 +37,7 @@ import static org.eclipse.edc.demo.tests.TestConstants.TEST_TIMEOUT_DURATION;
 @EndToEndTest
 public class CredentialIssuanceEndToEndTest {
 
-    private static final String CONSUMER_IDENTITYHUB_IDENTITY_URL = "http://ih.consumer.localhost:8080/cs/";
+    private static final String CONSUMER_IDENTITYHUB_IDENTITY_URL = "http://ih.consumer.localhost:8080/cs";
     private static final String PARTICIPANT_CONTEXT_ID = "consumer-participant";
     private static final String ISSUER_DID = "did:web:issuerservice.issuer.svc.cluster.local%3A10016:issuer";
     private static final String HOLDER_PID = UUID.randomUUID().toString();
